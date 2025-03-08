571b5f6ba14c5bae3f89c05b0d93b5a75f06e7d5
unknown <XX912@ccs.local>
2025-03-08 15:01:08 +0800

    update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('rootpath')();
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const errorHandler = require("_middlerware/error-handler");
+const errorHandler = require("_middleware/error-handler");
 
 app.use(express.json());
-app.use(express.urlrencoded({ extended: true }));
-app,use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // api routes
-app.use("/users" , require("./users/users.controller"));
+app.use("/users" , require("./users/users.controller").default);
 
 //global error handler
 
